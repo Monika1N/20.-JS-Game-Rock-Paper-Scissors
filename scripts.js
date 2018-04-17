9243d4b74ad7b82e4b5569612e8e9d3bbb8145b0
Monika N <monia.niemczyk1@gmail.com>
2018-04-17 14:35:21 +0200

    Displaying game elements

--- scripts.js
+++ scripts.js
@@ -19,5 +19,29 @@ var gameState = 'notStarted',  //started // ended
         score: 0
     };
 
+var newGameElem = document.getElementById('js-newGameElement'),
+    pickElem = document.getElementById('js-playerPickElement'),
+    resultsElem = document.getElementById('js-resultsTableElement');
+
+function setGameElements() {
+  switch(gameState) {
+    case 'started':
+        newGameElem.style.display = 'none';
+        pickElem.style.display = 'block';
+        resultsElem.style.display = 'block';
+      break;
+    case 'ended':
+        newGameBtn.innerText = 'Play again';
+    case 'notStarted':
+    default:
+        newGameElem.style.display = 'block';
+        pickElem.style.display = 'none';
+        resultsElem.style.display = 'none';
+  }
+}
+
+setGameElements();
+
+
 
 
